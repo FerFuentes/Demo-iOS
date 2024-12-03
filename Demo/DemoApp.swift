b9bcee6c65d7b259db12d526b501280d6d3c5251
ferfuentes <lzfer1@me.com>
2024-12-03 10:03:36 -0800

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Fernando Fuentes on 03/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
